9ac3ecea098534f3c28016bc292c1b4eb24a3538
Georgi Chalakov <gchalakovmmi@gmail.com>
2024-02-18 15:51:38 +0200

    Bug fix. Files didn't get unattached, now they do.

--- html/tasks/note_unattach_file.inc.php
+++ html/tasks/note_unattach_file.inc.php
@@ -5,5 +5,7 @@ require 'includes/php_auth_check.php';
 $note_id =$_GET['note_id'];
 $file_id =$_GET['file_id'];
 
+unattach_file_from_note($con, $file_id, $note_id, $_SESSION['user-details']['id']);
+
 header("location: /tasks/note_view.php?id=$note_id&status=success-file-unattached");
 exit;

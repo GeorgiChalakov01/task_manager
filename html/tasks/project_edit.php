4b196c628d4cb6ebb97e082bb6c8346be9afa01c
Georgi Chalakov <gchalakovmmi@gmail.com>
2024-02-18 16:12:56 +0200

    Fixed the project_edit page, now it works.

--- html/tasks/project_edit.php
+++ html/tasks/project_edit.php
@@ -64,7 +64,7 @@ if (isset($_GET['id'])) {
 						id="deadline" 
 						name="deadline" 
 						class="form-control"
-						value="<?php if($edit) echo $chosen_project['deadline']; ?>"
+						value="<?php if($edit) echo date('Y-m-d', strtotime($chosen_project['deadline'])); ?>"
 					>
 				</div>
 

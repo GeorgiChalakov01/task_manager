eb530c6ade139000005cfbf1eaad9de5e7945774
Georgi Chalakov <gchalakovmmi@gmail.com>
2024-04-04 23:50:32 +0300

    The database is now accessable through a user called TM instead of root. Created a setup.sh script to make installing easier.

--- html/common/db/db.conf.php
+++ html/common/db/db.conf.php
@@ -1,7 +1,7 @@
 <?php
 
 $db_ip='localhost';
-$db_user='root';
+$db_user='TM';
 $db_password='0000';
 $db_database='TM';
 $db_port=3306;
---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo "\n\nInstalling MariaDB..."
+sudo apt install mariadb-server
+
+echo "\n\nInstall PHP and php-mysqli..."
+sudo apt install php php-mysqli
+
+echo "\n\nSetup the database..."
+cd db_scripts
+sudo mariadb < 0_execute_all.sql
+cd ..
+
+echo -e "\n\nYou are now set.\nYou can host the contents of the html folder to get access to the web frontend.\nFor testing purposes you can do:\nphp -S localhost:8080 in the html directory."

0fece8c27a8ef84448e14572c8f96a3d8ceb676e
Georgi Chalakov <gchalakovmmi@gmail.com>
2024-04-08 12:50:15 +0300

    DB Bug fix.

--- db_scripts/0_live_upgrade.sql
+++ db_scripts/0_live_upgrade.sql
@@ -2,13 +2,13 @@ USE TM;
 
 DROP TABLE PHRASES;
 DROP TABLE LANGUAGES;
-SOURCE 1_create_language_tables.sql;
-SOURCE 2_populate_language_tables.sql;
+SOURCE 2_create_language_tables.sql;
+SOURCE 3_populate_language_tables.sql;
 
 DROP TABLE COLOR_SCHEMES;
-SOURCE 3_create_color_schemes.sql;
-SOURCE 4_populate_color_schemes.sql;
+SOURCE 4_create_color_schemes.sql;
+SOURCE 5_populate_color_schemes.sql;
 
 SOURCE 0_create_drop_all_procedures.sql;
 CALL DROP_ALL_PROCEDURES();
-SOURCE 6_create_procedures.sql;
+SOURCE 7_create_procedures.sql;

8cf73ea7c8c6bae593f5649824dc9dcbd612515c
Georgi Chalakov <gchalakovmmi@gmail.com>
2024-04-08 13:28:14 +0300

    DB adjustment.

--- db_scripts/0_live_upgrade.sql
+++ db_scripts/0_live_upgrade.sql
@@ -5,10 +5,6 @@ DROP TABLE LANGUAGES;
 SOURCE 2_create_language_tables.sql;
 SOURCE 3_populate_language_tables.sql;
 
-DROP TABLE COLOR_SCHEMES;
-SOURCE 4_create_color_schemes.sql;
-SOURCE 5_populate_color_schemes.sql;
-
 SOURCE 0_create_drop_all_procedures.sql;
 CALL DROP_ALL_PROCEDURES();
 SOURCE 7_create_procedures.sql;

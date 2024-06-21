5940e1a32323c9df3fc7406242d0fbef83921c9d
Georgi Chalakov <gchalakovmmi@gmail.com>
2024-06-21 21:35:24 +0300

    ...

--- nginx/html/common/minio/credentials.php
+++ nginx/html/common/minio/credentials.php
@@ -2,7 +2,7 @@
 
 $version = 'latest';
 $region = 'us-east-1';
-$endpoint = 'http://172.17.0.4:9000';
-$key = 'dpv0VJHaSMwaMtwpMliQ'; 
-$secret = 'MGVQ5vuV9C11Re1zd2jQCOwNN16lOnjuOKL358MG';
+$endpoint = 'http://task_manager_minio:9000';
+$key = 'YoGRoot'; 
+$secret = 'HayouSupMyMan?UFineTodaii?!?@O';
 $files_bucket = 'files';

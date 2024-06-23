d4e42268a3f03cb3160cc11a0317da787ff78de4
Georgi Chalakov <gchalakov@gmail.com>
2024-06-24 01:53:20 +0300

    Fixed the bug with finishing a task with the wrong id.

--- nginx/html/tasks/schedule.php
+++ nginx/html/tasks/schedule.php
@@ -38,7 +38,7 @@ $task_width = 300;
 				<!-- Date Choosing Container -->
 				<div class="bg-dark rounded text-white" style="height: 10%; width: 100%; padding: 0; margin: 0; border: 1px solid white;">
 					<input id="date-picker" style="float:right;" type="date"/>
-        </div>
+		</div>
 
 				<!-- Top Content Schedule Container -->
 			<div class="row rounded" style="height: 90%; padding: 0; margin: 0; overflow-x: hidden; overflow-y: auto; border: 1px solid white; width: auto;">
@@ -79,7 +79,7 @@ $task_width = 300;
 									<div 
 										class="rounded" 
 										style="font-size: 12px; text-align: center; position: absolute; border: 1px solid ' . $text_color . '; background-color: ' . $background_color . '; color: ' . $text_color . '; top: ' . $start_minutes . 'px; height: ' . $height . 'px; width: ' . $task_width . 'px; left: ' . (int)$scheduled_task['col'] * $task_width . 'px; cursor: pointer; z-index: ' . $scheduled_task['id'] . ';"
-										onclick="show_menu(' . $task['id'] . ', \'scheduled-task\', '. $scheduled_task['task_schedule_id'] .');"
+										onclick="show_menu(' . $scheduled_task['id'] . ', \'scheduled-task\', '. $scheduled_task['task_schedule_id'] .');"
 									>' . 
 										$scheduled_task['title'] . '
 									</div>';
@@ -93,20 +93,20 @@ $task_width = 300;
 			<!-- Empty Top Space Container -->
 			<div style="height: 3%; padding: 0; margin: 0;"></div>
 
-<!-- Top Task Container -->
-<div class="bg-dark rounded" style="height: 37%; padding: 0; margin: 0; overflow-x: auto;">
-    <!-- Project Choosing Container -->
-    <div class="bg-dark rounded text-white" style="height: 20%; padding: 5px; margin: 0; border: 1px solid white;">
-	<div style="width: 99.8%;overflow-x: auto; overflow-y: hidden;">
-        <div class="d-flex flex-nowrap" style="height: 90%;">
-            <?php
-            $projects = get_projects($con, $_SESSION['user-details']['id']);
-            foreach($projects as $project){ ?>
-                <a href="schedule.php?project_id=<?php echo $project['id']; ?>" class="rounded flex-shrink-0" style="background-color: <?php echo $project['background_color'];?>; color: <?php echo $project['text_color'];?>; text-decoration: none; padding: 2px; margin-right: 5px;" href=""><?php echo $project['title']=='default-project-title'?$phrases[$project['title']]:$project['title'];?></a>
-            <?php } ?>
-        </div>
-        </div>
-    </div>
+			<!-- Top Task Container -->
+			<div class="bg-dark rounded" style="height: 37%; padding: 0; margin: 0; overflow-x: auto;">
+				<!-- Project Choosing Container -->
+				<div class="bg-dark rounded text-white" style="height: 20%; padding: 5px; margin: 0; border: 1px solid white;">
+				<div style="width: 99.8%;overflow-x: auto; overflow-y: hidden;">
+				<div class="d-flex flex-nowrap" style="height: 90%;">
+					<?php
+					$projects = get_projects($con, $_SESSION['user-details']['id']);
+					foreach($projects as $project){ ?>
+					<a href="schedule.php?project_id=<?php echo $project['id']; ?>" class="rounded flex-shrink-0" style="background-color: <?php echo $project['background_color'];?>; color: <?php echo $project['text_color'];?>; text-decoration: none; padding: 2px; margin-right: 5px;" href=""><?php echo $project['title']=='default-project-title'?$phrases[$project['title']]:$project['title'];?></a>
+					<?php } ?>
+				</div>
+				</div>
+				</div>
 
 				<!-- Task Choosing Container -->
 				<div class="bg-light rounded row" style="height: 80%; padding: 0; margin: 0; overflow: auto; border: 1px solid white;">

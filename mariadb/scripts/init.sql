41028ba2e9cc97f33f5ac0ea36faba0483481a9b
Georgi Chalakov <gchalakov@gmail.com>
2024-06-24 01:26:32 +0300

    Created a test account. Email: test@test.test Password: test

--- mariadb/scripts/init.sql
+++ mariadb/scripts/init.sql
@@ -1,10 +1,10 @@
-SOURCE /docker-entrypoint-initdb.d/sql/1_prepare_db.sql;
-
 USE TM
-SOURCE /docker-entrypoint-initdb.d/sql/2_create_sequences.sql
-SOURCE /docker-entrypoint-initdb.d/sql/3_create_language_tables.sql
-SOURCE /docker-entrypoint-initdb.d/sql/4_populate_language_tables.sql
-SOURCE /docker-entrypoint-initdb.d/sql/5_create_color_schemes.sql
-SOURCE /docker-entrypoint-initdb.d/sql/6_populate_color_schemes.sql
-SOURCE /docker-entrypoint-initdb.d/sql/7_create_task_tables.sql
-SOURCE /docker-entrypoint-initdb.d/sql/8_create_procedures.sql
+
+SOURCE /docker-entrypoint-initdb.d/sql/1_create_sequences.sql
+SOURCE /docker-entrypoint-initdb.d/sql/2_create_language_tables.sql
+SOURCE /docker-entrypoint-initdb.d/sql/3_populate_language_tables.sql
+SOURCE /docker-entrypoint-initdb.d/sql/4_create_color_schemes.sql
+SOURCE /docker-entrypoint-initdb.d/sql/5_populate_color_schemes.sql
+SOURCE /docker-entrypoint-initdb.d/sql/6_create_task_tables.sql
+SOURCE /docker-entrypoint-initdb.d/sql/7_create_procedures.sql
+SOURCE /docker-entrypoint-initdb.d/sql/8_create_test_user.sql
